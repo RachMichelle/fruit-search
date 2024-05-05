192c9d918cb5521a20901f1a0cd60caa3d087723
Rachel <rachel@LAPTOP-2HAP8AE4.localdomain>
2024-05-05 12:03:33 -0400

    filled in js functions, finished styling

--- script.js
+++ script.js
@@ -4,24 +4,39 @@ const suggestions = document.querySelector('.suggestions ul');
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 function search(str) {
-	let results = [];
-
-	// TODO
-
-	return results;
+	const lowerCasedStr = str.toLowerCase();
+	const lowerCasedFruit = fruit.map(item => {
+		return item.toLowerCase();})
+	const result = lowerCasedFruit.filter(val => {
+		return val.includes(lowerCasedStr);
+	});
+	return result.map(resultItem => {
+		return resultItem.charAt(0).toUpperCase() + resultItem.slice(1);
+	})
 }
 
 function searchHandler(e) {
-	// TODO
+	suggestions.innerHTML = '';
+	showSuggestions();
 }
 
 function showSuggestions(results, inputVal) {
-
-	// TODO
+	inputVal = input.value;
+	results = search(inputVal);
+	if (inputVal === ''){
+		return;
+	}
+	for (let item of results){
+		let newLI = document.createElement('li');
+		newLI.classList.add('suggestedFruit');
+		newLI.innerText = item;
+		suggestions.appendChild(newLI);
+	}
 }
 
 function useSuggestion(e) {
-	// TODO
+	input.value = e.target.innerText;
+	suggestions.innerHTML = '';
 }
 
 input.addEventListener('keyup', searchHandler);
